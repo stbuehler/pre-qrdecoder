824a554f6e4faef80ba800c5d7a7e977e9a67ff1
Stefan Bühler <stbuehler@web.de>
2011-10-19 11:37:04 +0200

    use enyo "html highlight" if available

--- de.stbuehler.qrdecoder/app/models/highlight.js
+++ de.stbuehler.qrdecoder/app/models/highlight.js
@@ -14,7 +14,11 @@ function highlight(text, barcodeformat) {
 	default:
 		break;
 	}
-	text = text.replace(new RegExp('(https?://|mailto:)\\S*?\.?($|\\s)', 'ig'), function(value) { return '<a href="'+value+'">'+value+'</a>' });
+	if (enyo && enyo.string && enyo.string.runTextIndexer) {
+		text = enyo.string.runTextIndexer(text);
+	} else {
+		text = text.replace(new RegExp('(https?://|mailto:)\\S*?\.?($|\\s)', 'ig'), function(value) { return '<a href="'+value+'">'+value+'</a>' });
+	}
 	text = text.replace('\n', '<br />\n');
 	return text;
 }
